2f64f55fdebcb69c677b582b013f88ad4760d544
ChiniGumy <dsebastiansr@hotmail.com>
2023-10-07 11:03:50 -0500

    added microanimations on Register screen

--- src/routes/Register.jsx
+++ src/routes/Register.jsx
@@ -1,37 +1,34 @@
 function Register() {
   return (
-    <div className="bg-bg-black h-screen flex items-center justify-center p-20">
+    <div className="bg-bg-black h-screen flex items-center justify-center">
 
         <div className="flex flex-col">
 
-            <h1 className="text-sexy-green text-center text-3xl font-bold">REGISTER</h1>
+            <h1 className="text-sexy-green text-center text-3xl mb-6 font-bold">REGISTER</h1>
 
-            <div className="">
-                <div className="flex mb-4">
-                    <p className="text-xl text-white-text mr-4">Name</p>
-                    <input type="text" className="border-0 bg-transparent border-b border-white-strokes mb-2" />
+            <div className="w-[520px]">
+                <div className="flex mb-4 justify-between">
+                    <p className="text-xl w-[25%] text-left text-white-text mr-4">Name</p>
+                    <input type="text" className="border-0 bg-transparent border-b border-white-strokes mb-2 w-[75%] p-2 text-white-text outline-none focus:border-sexy-green transition-all"/>
                 </div>
-                <div className="flex mb-4">
-                    <p className="text-xl text-white-text mr-4">Last Name</p>
-                    <input type="text" className="border-0 bg-transparent border-b border-white-strokes mb-2" />
+                <div className="flex mb-4 justify-between">
+                    <p className="text-xl text-white-text w-[25%] mr-4">Last Name</p>
+                    <input type="text" className="border-0 bg-transparent border-b border-white-strokes mb-2 w-[75%] p-2 text-white-text outline-none focus:border-sexy-green transition-all"/>
                 </div>
-                <div className="flex mb-4">
-                    <p className="text-xl text-white-text mr-4">E-mail</p>
-                    <input type="text" className="border-0 bg-transparent border-b border-white-strokes mb-2" />
+                <div className="flex mb-4 justify-between">
+                    <p className="text-xl text-white-text text-left mr-4 w-[25%]">E-mail</p>
+                    <input type="text" className="border-0 bg-transparent border-b border-white-strokes mb-2 w-[75%] p-2 text-white-text outline-none focus:border-sexy-green transition-all"/>
                 </div>
-                <div className="flex mb-4">
-                    <p className="text-xl text-white-text mr-4">Nickname</p>
-                    <input type="text" className="border-0 bg-transparent border-b border-white-strokes mb-2" />
-                </div>
-                <div className="flex mb-4">
-                    <p className="text-xl text-white-text mr-4">Password</p>
-                    <input type="text" className="border-0 bg-transparent border-b border-white-strokes mb-2" />
-                </div>
+                <div className="flex mb-4 justify-between">
+                    <p className="text-xl text-white-text w-[25%] text-left mr-4">Nickname</p>
+                    <input type="text" className="border-0 bg-transparent border-b border-white-strokes mb-2 w-[75%] p-2 text-white-text outline-none focus:border-sexy-green transition-all"/>                </div>
+                <div className="flex mb-4 justify-between">
+                    <p className="text-xl text-white-text w-[25%] text-left mr-4">Password</p>
+                    <input type="password" className="border-0 bg-transparent border-b border-white-strokes mb-2 w-[75%] p-2 text-white-text outline-none focus:border-sexy-green transition-all"/>                </div>
                 <div className="flex mb-8">
-                    <p className="text-xl text-white-text mr-4">Confirm Password</p>
-                    <input type="text" className="border-0 bg-transparent border-b border-white-strokes mb-2" />
-                </div>
-                <button className="bg-sexy-green w-full p-2 rounded-[5px] text-[#CCC]">Register</button>
+                    <p className="text-xl text-white-text w-[25%] text-left mr-4">Confirm Password</p>
+                    <input type="password" className="border-0 bg-transparent border-b border-white-strokes mb-2 w-[75%] p-2 text-white-text outline-none focus:border-sexy-green transition-all"/>                </div>
+                <button className="bg-sexy-green w-full p-4 rounded-[5px] text-[#CCC]">Register</button>
             </div>
 
             <div className="mt-6 flex items-center">
